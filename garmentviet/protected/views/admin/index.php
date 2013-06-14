9fb3cbdc7653e84a7823782bd64353a03f4d8843
unglevan <huythang0312@gmail.com>
2013-06-14 17:37:24 +0700

    add admin index

---
+++ garmentviet/protected/views/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$this->menu=array(
+	array('label'=>'List News', 'url'=>array('/admin/NewsIndex')),
+	array('label'=>'Create News', 'url'=>array('/admin/NewsCreate')),
+);
