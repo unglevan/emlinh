71d9a28223a57c64ef27682908349f8883a1ae7f
linh nguyen <fire271092@gmail.com>
2013-06-22 01:16:51 +0700

    location

--- protected/views/location/index.php
+++ protected/views/location/index.php
@@ -86,4 +86,26 @@
             </div>  
         </div>
     </body>
+    <script>
+$(document).ready(function() {
+    if(<?php echo $checkPopUp ?> == true){
+	  $('#myModal').reveal(
+	  {
+		animation: 'none',                   //fade, fadeAndPop, none
+		animationspeed: 300,                       //how fast animtions are
+		closeonbackgroundclick: true,              //if you click background will modal close?
+		dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
+	});
+
+    }
+});
+</script>
+<div id="myModal" class="reveal-modal" >
+	<h1>NewLetter</h1>
+        <form action="" method="post">
+    <input type="text" name="email">
+    <input type="submit">
+    </form>
+	<a class="close-reveal-modal">&#215;</a>
+</div>
 </html>
